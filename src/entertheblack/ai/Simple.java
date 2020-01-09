289ef352854718e58236bcc6744a89925d3add1d
IntegratedQuantum <jahe788@gmail.com>
2020-01-09 16:10:24 +0100

    Fix a few bugs with the 'Simple' AI.

--- src/entertheblack/ai/Simple.java
+++ src/entertheblack/ai/Simple.java
@@ -18,17 +18,18 @@ public class Simple implements AI {
 		// Simulate own and enemy movement for the time the laser would need to go the current distance between this and enemy. Taking into account "doppler effect" on projectile "wavelength".
 		
 		// Calculate projection of enemy movement vector on (normalized) distance vector with scalar product:
-		double vRel = en.vmax*(en.vx*deltaX/dist + en.vy*deltaY/dist);
+		double vRel = (en.vx*deltaX/dist + en.vy*deltaY/dist);
 		double t = dist/(sh.v.wd1.velocity/10 - vRel);
-		// Simulate ship movement during given time:
-		deltaX += sh.vmax*sh.vx*t - en.vmax*en.vx*t;
-		deltaY += sh.vmax*sh.vy*t - en.vmax*en.vy*t;
+		// Simulate enemy ship movement during given time:
+		deltaX -= en.vx*t;
+		deltaY -= en.vy*t;
 		
 		double alpha = Math.atan(deltaY/deltaX);
 		if(deltaX < 0)
 			alpha += Math.PI;
 		if(alpha < 0)
 			alpha += 2*Math.PI;
+		
 		double flightTime1 = sh.v.wd1.range/sh.v.wd1.velocity*10; // Maximum Flight time of primary weapon.
 		if(t < flightTime1/3 && dist < sh.v.wd1.range/3) { // Only flee when enemy is actually close
 			// Flee
@@ -68,19 +69,31 @@ public class Simple implements AI {
 				g.rocketshoot2 = true;
 		}
 		else {
-			double deltaalpha = alpha - sh.alpha - Math.PI/2;
-			if(deltaalpha < -Math.PI)
-				deltaalpha += 2*Math.PI;
-			if(deltaalpha > Math.PI)
-				deltaalpha -= 2*Math.PI;
-			if(Math.abs(deltaalpha) > 0.01) {
-				if(deltaalpha < 0)
-					g.turnleft2 = true;
-				else
-					g.turnright2 = true;
-			}
-			if(Math.abs(deltaalpha) < 0.5)
-				g.move2 = true;
+			// Approach
+			approach(sh, en, g);
 		}
+	}
+	public void approach(Ship sh, Ship en, Game g) {
+		double deltaX = sh.x - en.x;
+		double deltaY = sh.y - en.y;
+		double alpha = Math.atan(deltaY/deltaX);
+		if(deltaX < 0)
+			alpha += Math.PI;
+		if(alpha < 0)
+			alpha += 2*Math.PI;
+		
+		double deltaalpha = alpha - sh.alpha - Math.PI/2;
+		if(deltaalpha < -Math.PI)
+			deltaalpha += 2*Math.PI;
+		if(deltaalpha > Math.PI)
+			deltaalpha -= 2*Math.PI;
+		if(Math.abs(deltaalpha) > 0.01) {
+			if(deltaalpha < 0)
+				g.turnleft2 = true;
+			else
+				g.turnright2 = true;
+		}
+		if(Math.abs(deltaalpha) < 0.5)
+			g.move2 = true;
 	}
 }
